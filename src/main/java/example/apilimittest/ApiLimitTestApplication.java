19bf72379e5f1510f2e55c08c53cc0f3728ddbd8
minsung gwak <131960511+opinionist@users.noreply.github.com>
2025-06-25 09:17:56 +0900

    api limit test 시작

---
+++ src/main/java/example/apilimittest/ApiLimitTestApplication.java
@@ -0,0 +1,13 @@
+package example.apilimittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLimitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiLimitTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/example/apilimittest/ApiLimitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package example.apilimittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLimitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
